9f7509897bd281310977820f91830429af7aa718
ruba dayy <ruba.dayyeh@gmail.com>
2024-06-26 14:29:10 +0300

    migrations

---
+++ Migrations/20240612104259_employeeswithrestaurant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservationDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class employeeswithrestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW employeeswithrestaurants
+            AS SELECT Employees.EmployeeId ,Employees.Position,
+            Employees.Firstname + Employees.Lastname as EmployeeName,
+            Restaurants.RestaurantId,Restaurants.Name,Restaurants.Address
+            FROM Employees LEFT JOIN Restaurants ON Restaurants.RestaurantId=Employees.RestaurantId
+            
+            ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW IF EXISTS employeeswithrestaurants;");
+        }
+    }
+}
